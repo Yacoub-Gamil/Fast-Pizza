8397ad63fa6429962ab81c2b6b1783ab3ed7c996
Yacoub-Gamil <yacoub-gamil@hotmail.com>
2024-09-15 15:11:48 +0300

    implemented the structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/older/CreateOrder";
+import Order from "./features/older/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>s</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
